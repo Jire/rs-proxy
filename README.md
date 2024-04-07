eb6c19257da716af4f3b88a8314bdf8fbaf36c08
Jire <thomasgnappo@gmaIl.com>
2024-04-07 02:29:41 -0400

    made it for OSRS

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# tcpproxy
-_a simple, cross-platform, multi-client TCP proxy_
+# tcpproxy-rs
+_a simple, cross-platform, multi-client TCP proxy for Old-school RS2/JS5_
 
-`tcpproxy` is a cross-platform, multi-client TCP proxy written in rust, that is designed for those "one-time" tasks where you usually end up spending more time installing a proxy server and setting up the myriad configuration files and options than you do actually using it.
+`tcpproxy-rs` is a cross-platform, multi-client TCP proxy written in rust, that is designed for those "one-time" tasks where you usually end up spending more time installing a proxy server and setting up the myriad configuration files and options than you do actually using it.
 
-`tcpproxy` is completely asynchronous and built on top of the `tokio` async runtime. It was written to serve as an example of how bi-directional async networking code using rust futures and an async framework would look and is intentionally kept easy to understand. The code is updated regularly to take advantage of new tokio features and best practices (if/when they change).
+`tcpproxy-rs` is completely asynchronous and built on top of the `tokio` async runtime. It was written to serve as an example of how bi-directional async networking code using rust futures and an async framework would look and is intentionally kept easy to understand. The code is updated regularly to take advantage of new tokio features and best practices (if/when they change).
 
 ## Usage
 
-`tcpproxy` is a command-line application. One instance of `tcpproxy` should be started for each remote endpoint you wish to proxy data to/from. All configuration is done via command-line arguments, in keeping with the spirit of this project.
+`tcpproxy-rs` is a command-line application. One instance of `tcpproxy-rs` should be started for each remote endpoint you wish to proxy data to/from. All configuration is done via command-line arguments, in keeping with the spirit of this project.
 
 ```
-tcpproxy REMOTE_HOST:PORT [-b BIND_ADDR] [-l LOCAL_PORT]
+tcpproxy-rs VERSION REMOTE_HOST:PORT [-b BIND_ADDR] [-l LOCAL_PORT]
 
 Options:
     -b, --bind BIND_ADDR
@@ -26,20 +26,6 @@ Where possible, sane defaults for arguments are provided automatically.
 
 ## Installation
 
-`tcpproxy` is available via `cargo`, the rust package manager. Installation is as follows:
+`tcpproxy-rs` is available via `cargo`, the rust package manager. Installation is as follows:
 
-    cargo install tcpproxy
-
-Pre-complied binaries for select platforms may be available from the `tcpproxy` homepage at https://neosmart.net/tcpproxy/
-
-## Project Status
-
-Depending on which language ecosystem you are coming from, this project may appear to be "unmaintained." Do not be fooled by a lack of updates for some length of time - this project is regularly updated **when needed** to fix bugs, improve code quality, use more modern rust coding patterns and conventions, and update dependencies. This project is *not*, however, updated for the sake of updating and is currently, in the humble opinion of its author, fairly feature-complete. The intention was always to provide a minimalistic (but still useful!) tcp proxy that can be quickly fired-up from the command line and put to good use. It is not intended to become comprehensive of any and all peripheral features and attempts to bundle "everything and the kitchen sink" will be respectfully but firmly declined.
-
-## Contributing
-
-Pull requests are welcome, but for any major undertakings, please do open an issue first to make sure we're all on the same page!
-
-## License and Authorship
-
-`tcpproxy` is developed and maintained by Mahmoud Al-Qudsi of NeoSmart Technologies. `tcpproxy` is open source and licensed under the terms of the MIT public license, made available to the general public without warranty in the hopes that it may prove both edifying and useful.
+    cargo install tcpproxy-rs
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+use std::env;
+use std::net::SocketAddr;
+use std::sync::atomic::{AtomicBool, Ordering};
+
 use futures::FutureExt;
 use getopts::Options;
-use std::env;
-use std::sync::atomic::{AtomicBool, Ordering};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::broadcast;
 
 type BoxedError = Box<dyn std::error::Error + Sync + Send + 'static>;
+
 static DEBUG: AtomicBool = AtomicBool::new(false);
 const BUF_SIZE: usize = 1024;
 
@@ -14,7 +17,7 @@ fn print_usage(program: &str, opts: Options) {
     let program_path = std::path::PathBuf::from(program);
     let program_name = program_path.file_stem().unwrap().to_string_lossy();
     let brief = format!(
-        "Usage: {} REMOTE_HOST:PORT [-b BIND_ADDR] [-l LOCAL_PORT]",
+        "Usage: {} VERSION REMOTE_HOST:PORT [-b BIND_ADDR] [-l LOCAL_PORT]",
         program_name
     );
     print!("{}", opts.usage(&brief));
@@ -23,9 +26,21 @@ fn print_usage(program: &str, opts: Options) {
 #[tokio::main]
 async fn main() -> Result<(), BoxedError> {
     let args: Vec<String> = env::args().collect();
+
     let program = args[0].clone();
 
+    let version_str = &args[1]; // Get the first user-supplied argument
+    let version: u32 = version_str.parse().map_err(|e| format!("Failed to parse version as u32: {}", e))?;
+
+    println!("Version: {}", version);
+
     let mut opts = Options::new();
+    opts.reqopt(
+        "v",
+        "version",
+        "The RuneScape client version.",
+        "VERSION",
+    );
     opts.optopt(
         "b",
         "bind",
@@ -40,7 +55,7 @@ async fn main() -> Result<(), BoxedError> {
     );
     opts.optflag("d", "debug", "Enable debug mode");
 
-    let matches = match opts.parse(&args[1..]) {
+    let matches = match opts.parse(&args[2..]) {
         Ok(opts) => opts,
         Err(e) => {
             eprintln!("{}", e);
@@ -48,6 +63,7 @@ async fn main() -> Result<(), BoxedError> {
             std::process::exit(-1);
         }
     };
+
     let remote = match matches.free.len() {
         1 => matches.free[0].clone(),
         _ => {
@@ -62,17 +78,23 @@ async fn main() -> Result<(), BoxedError> {
     }
 
     DEBUG.store(matches.opt_present("d"), Ordering::Relaxed);
+
     // let local_port: i32 = matches.opt_str("l").unwrap_or("0".to_string()).parse()?;
     let local_port: i32 = matches.opt_str("l").map(|s| s.parse()).unwrap_or(Ok(0))?;
+
     let bind_addr = match matches.opt_str("b") {
         Some(addr) => addr,
         None => "127.0.0.1".to_owned(),
     };
 
-    forward(&bind_addr, local_port, remote).await
+    forward(version, &bind_addr, local_port, remote).await
 }
 
-async fn forward(bind_ip: &str, local_port: i32, remote: String) -> Result<(), BoxedError> {
+async fn forward(
+    version: u32,
+    bind_ip: &str,
+    local_port: i32,
+    remote: String) -> Result<(), BoxedError> {
     // Listen on the specified IP and port
     let bind_addr = if !bind_ip.starts_with('[') && bind_ip.contains(':') {
         // Correctly format for IPv6 usage
@@ -81,7 +103,7 @@ async fn forward(bind_ip: &str, local_port: i32, remote: String) -> Result<(), B
         format!("{}:{}", bind_ip, local_port)
     };
     let bind_sock = bind_addr
-        .parse::<std::net::SocketAddr>()
+        .parse::<SocketAddr>()
         .expect("Failed to parse bind address");
     let listener = TcpListener::bind(&bind_sock).await?;
     println!("Listening on {}", listener.local_addr().unwrap());
@@ -94,25 +116,159 @@ async fn forward(bind_ip: &str, local_port: i32, remote: String) -> Result<(), B
     // (This reduces MESI/MOESI cache traffic between CPU cores.)
     let remote: &str = Box::leak(remote.into_boxed_str());
 
-    // Two instances of this function are spawned for each half of the connection: client-to-server,
-    // server-to-client. We can't use tokio::io::copy() instead (no matter how convenient it might
-    // be) because it doesn't give us a way to correlate the lifetimes of the two tcp read/write
-    // loops: even after the client disconnects, tokio would keep the upstream connection to the
-    // server alive until the connection's max client idle timeout is reached.
-    async fn copy_with_abort<R, W>(
-        read: &mut R,
-        write: &mut W,
-        mut abort: broadcast::Receiver<()>,
-    ) -> tokio::io::Result<usize>
+    loop {
+        let (mut client, client_addr) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            println!("New connection from {}", client_addr);
+
+            match client.read_u8().await {
+                Ok(14) => handle_rs2(remote, client, client_addr).await,
+                Ok(15) => handle_js5(version, remote, client, client_addr).await,
+                Ok(opcode) => {
+                    println!("Invalid opcode {} from {}", opcode, client_addr);
+                    return false;
+                }
+                Err(e) => {
+                    eprintln!("failed to read from socket; err = {:?}", e);
+                    return false;
+                }
+            }
+        });
+    }
+}
+
+async fn handle_rs2(
+    remote: &str,
+    mut client: TcpStream,
+    client_addr: SocketAddr,
+) -> bool {
+    match client.write_u8(0).await {
+        Ok(_) => {
+            println!("Connecting RS2 {}", client_addr);
+            return start_proxying(remote, client, client_addr, 14).await;
+        }
+        Err(e) =>
+            eprintln!("failed to write RS2 response to socket; err = {:?}", e)
+    }
+    return false;
+}
+
+async fn handle_js5(
+    expected_version: u32,
+    remote: &str,
+    mut client: TcpStream,
+    client_addr: SocketAddr,
+) -> bool {
+    match client.read_u32().await {
+        Ok(version) => if expected_version == version {
+            match client.write_u8(0).await {
+                Ok(_) => {
+                    println!("Connecting JS5 {}", client_addr);
+                    return start_proxying(remote, client, client_addr, 15).await;
+                }
+                Err(e) =>
+                    eprintln!("failed to write JS5 response to socket; err = {:?}", e)
+            }
+        },
+        Err(e) => eprintln!("failed to read from JS5 socket; err = {:?}", e)
+    };
+    return false;
+}
+
+async fn start_proxying(
+    remote: &str,
+    mut client: TcpStream,
+    client_addr: SocketAddr,
+    opcode: u8,
+) -> bool {
+    // Establish connection to upstream for each incoming client connection
+    let mut remote = match TcpStream::connect(remote).await {
+        Ok(result) => result,
+        Err(e) => {
+            eprintln!("Error establishing upstream connection: {e}");
+            return false;
+        }
+    };
+
+    // Write the opcode to the server
+    match remote.write_u8(opcode).await {
+        Ok(_) => println!("Connected {} with opcode {}", client_addr, opcode),
+        Err(e) => {
+            eprintln!("Failed to write opcode {} response to {}; err = {:?}", opcode, client_addr, e);
+            return false;
+        }
+    }
+
+    let (mut client_read, mut client_write) = client.split();
+    let (mut remote_read, mut remote_write) = remote.split();
+
+    let (cancel, _) = broadcast::channel::<()>(1);
+    let (remote_copied, client_copied) = tokio::join! {
+                copy_with_abort(&mut remote_read, &mut client_write, cancel.subscribe())
+                    .then(|r| { let _ = cancel.send(()); async { r } }),
+                copy_with_abort(&mut client_read, &mut remote_write, cancel.subscribe())
+                    .then(|r| { let _ = cancel.send(()); async { r } }),
+            };
+
+    match client_copied {
+        Ok(count) => {
+            if DEBUG.load(Ordering::Relaxed) {
+                eprintln!(
+                    "Transferred {} bytes from proxy client {} to upstream server",
+                    count, client_addr
+                );
+            }
+        }
+        Err(err) => {
+            eprintln!(
+                "Error writing bytes from proxy client {} to upstream server",
+                client_addr
+            );
+            eprintln!("{}", err);
+        }
+    };
+
+    match remote_copied {
+        Ok(count) => {
+            if DEBUG.load(Ordering::Relaxed) {
+                eprintln!(
+                    "Transferred {} bytes from upstream server to proxy client {}",
+                    count, client_addr
+                );
+            }
+        }
+        Err(err) => {
+            eprintln!(
+                "Error writing from upstream server to proxy client {}!",
+                client_addr
+            );
+            eprintln!("{}", err);
+        }
+    };
+
+    return true;
+}
+
+// Two instances of this function are spawned for each half of the connection: client-to-server,
+// server-to-client. We can't use tokio::io::copy() instead (no matter how convenient it might
+// be) because it doesn't give us a way to correlate the lifetimes of the two tcp read/write
+// loops: even after the client disconnects, tokio would keep the upstream connection to the
+// server alive until the connection's max client idle timeout is reached.
+async fn copy_with_abort<R, W>(
+    read: &mut R,
+    write: &mut W,
+    mut abort: broadcast::Receiver<()>,
+) -> tokio::io::Result<usize>
     where
         R: tokio::io::AsyncRead + Unpin,
         W: tokio::io::AsyncWrite + Unpin,
-    {
-        let mut copied = 0;
-        let mut buf = [0u8; BUF_SIZE];
-        loop {
-            let bytes_read;
-            tokio::select! {
+{
+    let mut copied = 0;
+    let mut buf = [0u8; BUF_SIZE];
+    loop {
+        let bytes_read;
+        tokio::select! {
                 biased;
 
                 result = read.read(&mut buf) => {
@@ -128,81 +284,15 @@ async fn forward(bind_ip: &str, local_port: i32, remote: String) -> Result<(), B
                 }
             }
 
-            if bytes_read == 0 {
-                break;
-            }
-
-            // While we ignore some read errors above, any error writing data we've already read to
-            // the other side is always treated as exceptional.
-            write.write_all(&buf[0..bytes_read]).await?;
-            copied += bytes_read;
+        if bytes_read == 0 {
+            break;
         }
 
-        Ok(copied)
+        // While we ignore some read errors above, any error writing data we've already read to
+        // the other side is always treated as exceptional.
+        write.write_all(&buf[0..bytes_read]).await?;
+        copied += bytes_read;
     }
 
-    loop {
-        let (mut client, client_addr) = listener.accept().await?;
-
-        tokio::spawn(async move {
-            println!("New connection from {}", client_addr);
-
-            // Establish connection to upstream for each incoming client connection
-            let mut remote = match TcpStream::connect(remote).await {
-                Ok(result) => result,
-                Err(e) => {
-                    eprintln!("Error establishing upstream connection: {e}");
-                    return;
-                }
-            };
-            let (mut client_read, mut client_write) = client.split();
-            let (mut remote_read, mut remote_write) = remote.split();
-
-            let (cancel, _) = broadcast::channel::<()>(1);
-            let (remote_copied, client_copied) = tokio::join! {
-                copy_with_abort(&mut remote_read, &mut client_write, cancel.subscribe())
-                    .then(|r| { let _ = cancel.send(()); async { r } }),
-                copy_with_abort(&mut client_read, &mut remote_write, cancel.subscribe())
-                    .then(|r| { let _ = cancel.send(()); async { r } }),
-            };
-
-            match client_copied {
-                Ok(count) => {
-                    if DEBUG.load(Ordering::Relaxed) {
-                        eprintln!(
-                            "Transferred {} bytes from proxy client {} to upstream server",
-                            count, client_addr
-                        );
-                    }
-                }
-                Err(err) => {
-                    eprintln!(
-                        "Error writing bytes from proxy client {} to upstream server",
-                        client_addr
-                    );
-                    eprintln!("{}", err);
-                }
-            };
-
-            match remote_copied {
-                Ok(count) => {
-                    if DEBUG.load(Ordering::Relaxed) {
-                        eprintln!(
-                            "Transferred {} bytes from upstream server to proxy client {}",
-                            count, client_addr
-                        );
-                    }
-                }
-                Err(err) => {
-                    eprintln!(
-                        "Error writing from upstream server to proxy client {}!",
-                        client_addr
-                    );
-                    eprintln!("{}", err);
-                }
-            };
-
-            ()
-        });
-    }
+    Ok(copied)
 }
